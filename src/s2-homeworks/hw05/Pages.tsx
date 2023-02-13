ca17717a0887f497a068b796dfb1118d85deafb0
alpetrushkin <alpetrushkin1984@mail.ru>
2023-02-13 21:33:49 +0300

    hw5dop_2

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -26,7 +26,7 @@ function Pages() {
                 <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route path={'/error404'} element={<Navigate to={'/error404'}/>}/>
+                <Route path={'/*'} element={<Error404/>}/>
             </Routes>
         </div>
     )
